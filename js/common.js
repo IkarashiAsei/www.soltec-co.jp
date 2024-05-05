1fb0cd7bfb6c15d39dc05a6315f846fb85fe2480
五十嵐亜星 <>
2024-05-05 23:49:07 +0900

    修正

---
+++ js/common.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var menuIcon = document.getElementById("menu-icon");
+    var menuSp = document.querySelector(".menu-sp");
+
+    // メニューアイコンをクリックしたときの処理
+    menuIcon.addEventListener("click", function () {
+        // メニューアイコンの active クラスをトグルする
+        this.classList.toggle("active");
+
+        // menu-spの表示状態を切り替える
+        menuSp.classList.toggle("active");
+    });
+});
