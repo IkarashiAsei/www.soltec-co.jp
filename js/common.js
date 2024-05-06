b12aa2a601acb3963900ffd128df0614d4f8728f
五十嵐亜星 <>
2024-05-06 14:27:10 +0900

    修正

--- js/common.js
+++ js/common.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var menuIcon = document.getElementById("menu-icon");
-    var menuSp = document.querySelector(".menu-sp");
+    var menuIcon = document.getElementById("menu-icon")
+    var menuSp = document.querySelector(".menu-sp")
 
     // メニューアイコンをクリックしたときの処理
     menuIcon.addEventListener("click", function () {
         // メニューアイコンの active クラスをトグルする
-        this.classList.toggle("active");
+        this.classList.toggle("active")
 
         // menu-spの表示状態を切り替える
-        menuSp.classList.toggle("active");
-    });
-});
+        menuSp.classList.toggle("active")
+    })
+})
